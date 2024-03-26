8ecd9749830eb84bd679b8d86b70c9cf2d0a08d6
NegiKareena <159912897+NegiKareena@users.noreply.github.com>
2024-03-26 23:05:03 +0530

    Add files via upload

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,34 @@
+const JWT = require('jsonwebtoken');
+
+module.exports = async (req, res, next) => {
+    try {
+        const token = req.headers['authorization'].split(" ")[1]; // Corrected split method
+
+        if (!token) {
+            return res.status(401).send({
+                success: false,
+                message: 'Auth Failed: No token provided'
+            });
+        }
+
+        JWT.verify(token, process.env.JWT_SECRET, (err, decode) => {
+            if (err) {
+                return res.status(401).send({
+                    success: false,
+                    message: 'Auth Failed: Invalid token'
+                });
+            } else {
+                req.body.userId = decode.userId; // Changed req.body.userId to req.userId
+                next();
+            }
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(401).send({
+            success: false,
+            error,
+            message: 'Auth Failed'
+        });
+    }
+};
